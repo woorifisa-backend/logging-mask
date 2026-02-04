e8a0efcee9b2c450add0a7030955b8844a6fcf70
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2026-02-04 16:06:25 +0900

    updated import Logger

--- LoggingMask/src/main/java/dev/logging/Main.java
+++ LoggingMask/src/main/java/dev/logging/Main.java
@@ -1,6 +1,8 @@
 package dev.logging;
 
 import dev.logging.app.LogSimulation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
